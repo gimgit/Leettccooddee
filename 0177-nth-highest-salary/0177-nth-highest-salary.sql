5753587861814cd480a35a9b25178cf0a65f3805
heekyung <85975904+gimgit@users.noreply.github.com>
2024-09-29 17:53:01 +0900

    Time: 401 ms (66.98%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,15 +1,11 @@
-CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
-result NUMBER;
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    /* Write your PL/SQL query statement below */
-    SELECT
-      MAX(salary) INTO result
-      FROM (
-        SELECT
-            salary,
-            DENSE_RANK() OVER (order by salary desc) rk
-         FROM Employee
-      ) re
-    WHERE re.rk = N;
-    RETURN result;
-END;
+  DECLARE SAL_RANK INT;
+  SET SAL_RANK = N - 1;
+  RETURN (
+    SELECT DISTINCT SALARY 
+    FROM EMPLOYEE 
+    ORDER BY SALARY DESC
+    LIMIT SAL_RANK, 1
+  );
+END
