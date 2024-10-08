cde753661bc6d92559fc0d6def3baacdfc989b61
heekyung <85975904+gimgit@users.noreply.github.com>
2024-10-08 21:53:10 +0900

    Time: 113 ms (97.41%), Space: 18.3 MB (28.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -9,14 +9,3 @@ class Solution:
                 zero_pointer += 1
 
 
-
-
-        # # slow = 0
-        # for num in range(len(nums)):
-        #     if nums[num] != 0 and nums[zero_pointer] == 0:
-        #         nums[zero_pointer], nums[num] = nums[num], nums[zero_pointer]
-
-        #     # wait while we find a non-zero element to
-        #     # swap with you
-        #     if nums[zero_pointer] != 0:
-        #         zero_pointer += 1
