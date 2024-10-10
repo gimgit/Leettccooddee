a21a0a516186a5bb6f897dbc84ce1ade2830cc15
heekyung <85975904+gimgit@users.noreply.github.com>
2024-10-10 22:58:55 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0475-heaters/0475-heaters.py
+++ 0475-heaters/0475-heaters.py
@@ -4,17 +4,6 @@ class Solution:
         heaters = sorted(heaters)
         radius = 0
         idx = 0
-
-        if len(heaters) == 1:
-            candidates = [
-                abs(heaters[0] - max(houses)),
-                abs(heaters[0] - min(houses))
-            ]
-            return (max(candidates))
-
-        if houses == heaters:
-            return 0
-
         for house in houses:
             while idx < len(heaters) -1 and  abs(heaters[idx] - house) > abs(heaters[idx + 1] - house):
                 idx += 1
