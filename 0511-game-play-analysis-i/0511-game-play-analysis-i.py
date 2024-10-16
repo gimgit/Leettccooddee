e975912e3dfa5e0330218fb6f2f8d8446b7fc121
heekyung <85975904+gimgit@users.noreply.github.com>
2024-10-16 23:58:32 +0900

    Time: 418 ms (40.24%), Space: 71 MB (22.42%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.py
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.py
@@ -4,4 +4,5 @@ def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     activity = activity[['player_id', 'event_date']]
     activity = activity.groupby(by=['player_id'], as_index=False)['event_date'].min()
     activity = activity.rename(columns={'event_date': 'first_login'})
+    
     return activity
