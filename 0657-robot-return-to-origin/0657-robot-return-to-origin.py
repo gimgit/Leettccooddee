4484e3a9068502c040903f87aca313320613a454
heekyung <85975904+gimgit@users.noreply.github.com>
2024-10-24 22:13:27 +0900

    Time: 39 ms (86.55%), Space: 14 MB (100%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        if len(moves) % 2 is not 0:
+            return False
+        else:
+            counter = Counter(moves)
+            isUpDownSame = counter['U'] == counter['D']
+            isLeftRightSame = counter['R'] == counter['L']
+            return  isUpDownSame and isLeftRightSame
